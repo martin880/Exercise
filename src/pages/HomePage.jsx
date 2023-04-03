bcddde6c521d99e0af19baedd85dfca7116429b6
Martin Halomoan Lumbangaol <martinlumbangaol880@gmail.com>
2023-04-03 10:21:12 +0700

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import Playbar from '../components/Playbar';
 
 export default function HomePage() {
   return (
     <div style={{ display: 'flex' }}>
       <Navbar />
       <Sidebar />
+      <Playbar />
     </div>
   );
 }
